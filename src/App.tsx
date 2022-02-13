f00018cb3ae8b407bcee8e81377d022c3008e091
Jakob Csörgei Gustavsson <jakob@sigmastocks.com>
2022-02-13 14:52:16 +0100

    Typing out letters into grid, with hooks!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { FunctionComponent, useEffect, useState } from 'react';
+
+
+const Grid: FunctionComponent<{word: string; guesses: string[]}> = ({word = "", guesses = []}) => {
+  return (
+    <table className="Grid">
+      <tbody>
+        {
+          [Array(6).fill(0).map((_, i:number) => <Row guess={guesses.slice(i*5, i*5+5)} key={i}/>)]
+        }
+      </tbody>
+    </table>
+  );
+}
+
+const Row: FunctionComponent<{guess: string[]}> = ({guess = []}) => {
+  return (
+    <tr>
+      {
+        [Array(5).fill(0).map((_, i: number) => <Cell letter={guess[i]} key={i}/>)]
+      }
+    </tr>
+  );
+}
+
+const Cell: FunctionComponent<{letter: string}> = ({letter = ""}) => {
+  return (
+      <td className="Cell">{letter}</td>
+  );
+}
+
+
+const App: FunctionComponent<{}> = () => {
+  const [guesses, setGuesses] = useState<string[]>([] as string[])
+  
+  useEffect(() => {
+    const handleKeydown = (event: any) => {
+      if (event.keyCode >= 65 && event.keyCode <= 90) {
+        const updatedGuess = guesses.slice();
+        updatedGuess.push(event.key.toUpperCase());
+        setGuesses(updatedGuess)
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+    return () => {
+      window.removeEventListener('keydown', handleKeydown)
+    }
+  })
+
+  const word = "HEJSAN";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid word={word} guesses={guesses}/>
     </div>
   );
 }
+
+
 
 export default App;
